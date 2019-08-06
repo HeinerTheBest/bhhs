b2eafbc2b9cc26c2fb244bf48d401205a66f9c42
Admin <heinerbauza@gmail.com>
2019-08-06 02:38:18 -0400

    show pictures of every house

--- app/src/main/java/com/mobileapps/bhhslux/viewmodel/ShowHousesViewModel.kt
+++ app/src/main/java/com/mobileapps/bhhslux/viewmodel/ShowHousesViewModel.kt
@@ -1,8 +1,12 @@
 package com.mobileapps.bhhslux.viewmodel
 
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.mobileapps.bhhslux.R
 import com.mobileapps.bhhslux.model.House
+import com.squareup.picasso.Picasso
 
 class ShowHousesViewModel : ViewModel
 {
@@ -30,24 +34,29 @@ class ShowHousesViewModel : ViewModel
 
     var arrayList = ArrayList<ShowHousesViewModel>()
 
+    fun getImageUrl():String{
+        return imagePatch
+    }
+
+
     fun getArrayList() : MutableLiveData<ArrayList<ShowHousesViewModel>>
     {
         val house1 = House("1","$1,350","1651 Massachussetts Avenue SW#17 Marietta, GA 30008-7166","For Sale - Active","4bd 3 ba","https://i.ytimg.com/vi/Xx6t0gmQ_Tw/maxresdefault.jpg")
-        val house2 = House("2","$399,900","1590 Arden Drive SW Marietta, GA 30008-3731","For Sale - Active","3bd 2 ba","http://nisartmacka.com/images/3.bp.blogspot.com/_X1QryFC2O28/TLDYMWzE4LI/AAAAAAAAAMM/14ZjpMgKveg/s1600/048.JPG")
+        val house2 = House("2","$399,900","1590 Arden Drive SW Marietta, GA 30008-3731","For Sale - Active","3bd 2 ba","https://assets.themortgagereports.com/wp-content/uploads/2017/12/How-to-Buy-a-House-with-Low-Income-This-Year.jpg")
         val house3 = House("3","$310,000","1651 Massachussetts Avenue SW#17 Marietta, GA 30008-7166","For Sale - Active","4bd 3 ba","https://cdn.decorpad.com/photos/2017/09/19/8e667843102e.jpg")
         val house4 = House("4","$295,775","1651 Massachussetts Avenue SW#17 Marietta, GA 30008-7166","For Sale - Active","4bd 3 ba","https://upload.wikimedia.org/wikipedia/commons/d/d8/SaltBoxHouse1.jpg")
 
 
 
-        val showHousesViewModel1 : ShowHousesViewModel = ShowHousesViewModel(house1)
-        val showHousesViewModel2 : ShowHousesViewModel = ShowHousesViewModel(house2)
-        val showHousesViewModel3 : ShowHousesViewModel = ShowHousesViewModel(house3)
-        val showHousesViewModel4 : ShowHousesViewModel = ShowHousesViewModel(house4)
+        val showHousesViewModel1 = ShowHousesViewModel(house1)
+        val showHousesViewModel2 = ShowHousesViewModel(house2)
+        val showHousesViewModel3 = ShowHousesViewModel(house3)
+        val showHousesViewModel4 = ShowHousesViewModel(house4)
 
-        arrayList!!.add(showHousesViewModel1)
-        arrayList!!.add(showHousesViewModel2)
-        arrayList!!.add(showHousesViewModel3)
-        arrayList!!.add(showHousesViewModel4)
+        arrayList.add(showHousesViewModel1)
+        arrayList.add(showHousesViewModel2)
+        arrayList.add(showHousesViewModel3)
+        arrayList.add(showHousesViewModel4)
 
         arrayListMutableLiveData.value = arrayList
 
@@ -57,4 +66,15 @@ class ShowHousesViewModel : ViewModel
     }
 
 
+}
+
+
+object ImageBindingAdapter
+{
+    @JvmStatic
+    @BindingAdapter("android:src")
+    fun setImageUrl(view:ImageView,url:String)
+    {
+        Picasso.with(view.context).load(url).placeholder(R.drawable.housedemo).into(view)
+    }
 }
