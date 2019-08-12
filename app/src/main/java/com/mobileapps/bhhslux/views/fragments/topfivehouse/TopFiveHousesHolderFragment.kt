7ace0a877e94a85755d28a79cd3c78d120e3a2b2
Admin <heinerbauza@gmail.com>
2019-08-12 09:40:26 -0400

    finished main view

--- app/src/main/java/com/mobileapps/bhhslux/views/fragments/topfivehouse/TopFiveHousesHolderFragment.kt
+++ app/src/main/java/com/mobileapps/bhhslux/views/fragments/topfivehouse/TopFiveHousesHolderFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.mobileapps.bhhslux.R
 import com.mobileapps.bhhslux.model.datasource.remote.mock.MockDataBase
+import com.mobileapps.bhhslux.views.fragments.housesdetail.HousesDetailFragment
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.fragment_main.view.*
 
@@ -22,13 +23,7 @@ class TopFiveHousesHolderFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         val rootView = inflater.inflate(R.layout.fragment_main, container, false)
-        /*since our views are in fragment_main.xml which is inflated in rootView
-          so we have to write rootView.oursomeview
-          otherwise it will try to find the view in activity_main.xml so app will crash*/
-        //handle swipe/slide
-
         val indexText = arguments!!.getInt(ARG_SECTION_NUMBER).toString()+" of 5"
-
 
         val houses = db.getHouses(5)
 
@@ -41,23 +36,23 @@ class TopFiveHousesHolderFragment : Fragment() {
                 rootView.tvDescription.text = houses[i].shortDescription
                 rootView.tvIndex.text = indexText
                 Picasso.with(context).load(houses[i].imagePatch).placeholder(R.drawable.housedemo).into(rootView.imgPicture)
+
+                rootView.setOnClickListener {
+                    val fragmentTransaction = activity?.supportFragmentManager?.beginTransaction()
+                    fragmentTransaction?.addToBackStack(null)
+                    val detailHouseFragment = HousesDetailFragment.newInstance(houses[i].id.toInt())
+                    fragmentTransaction?.replace(R.id.fragmentLayout,detailHouseFragment)
+                    fragmentTransaction?.commit()
+                }
+
             }
         }
         return rootView
     }
 
-    //Todo 04 the object class
     companion object {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
         private val ARG_SECTION_NUMBER = "section_number"
 
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
         fun newInstance(sectionNumber: Int): TopFiveHousesHolderFragment {
             val fragment = TopFiveHousesHolderFragment()
             val args = Bundle()
